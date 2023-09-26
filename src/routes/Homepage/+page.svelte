5f88d1160146b8f5ef00e8b59b8d618e5520e40e
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-09-26 16:31:49 +0530

    Add files via upload

---
+++ src/routes/Homepage/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Button from "$lib/Navbar/navbar.svelte";
+
+
+
+</script>
+
+
+
+<main>
+  <h1 class="h1" style="color:red;">Welcome to Hire optica</h1>
+  <div class="container">
+
+</div>
+</main>
+
+
+<Button/>
+
+
+<style>
+ 
+  .h1{
+    position: absolute;
+    top:200px;
+    left: 600px;
+    font-size:80px;
+  }
+  
+</style>
